9a4a1b6cf64df360ccb07e4778b2d13a15dc2019
Gogodr <gogodr@hotmail.com>
2022-04-28 05:07:47 -0500

    up app version

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ from modules.sound import playCheck, playError, playPulse, playSuccess
 
 from ui.config.config import LostArkMarketWatcherConfig
 
-version = '0.4.0'
+version = '0.4.1'
 
 
 class LostArkMarketWatcher(QApplication):
